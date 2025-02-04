d75c260df3adb50d835a1985a20f138b5c513f0b
x-gaming-renzov <paras@xgaming.club>
2025-02-04 13:52:43 +0530

    Restrict offers to include only Minecraft vanilla items in prompts

--- src/agents/experiment_agent/prompts/prompts.py
+++ src/agents/experiment_agent/prompts/prompts.py
@@ -33,6 +33,7 @@ But to give that, you need to create a correct item detials format which has all
 - You need to provide minecraft command to provide that item to user.
 - Keep player name as {{player_name}} in command str.
 - in case item is in-game currency, _set_command will be /give cash {{player_name}} {{amount}}
+- ONLY ADD MINECRAFT VANILLA ITEMS IN OFFER.
 
 # Here's the item to be provided : {item}
 # Here's the context of offer this item is part of : {offer_context}
