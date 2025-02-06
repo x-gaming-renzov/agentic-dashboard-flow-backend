4817493d140717368bcf66ac70c84a28ad0e70f0
jeeravan <pranaypandit12@gmail.com>
2025-02-06 19:28:14 +0530

    multi item

--- src/agents/item_agent/nodes/nodes.py
+++ src/agents/item_agent/nodes/nodes.py
@@ -20,13 +20,11 @@ print(colored(f"Status: ", "yellow"), colored(f"Initialising ChatOpenAI", "white
 model_large = ChatOpenAI(model="o1-preview")
 model = ChatOpenAI(model="gpt-4o-mini")
 print(colored(f"Status: ", "green"), colored(f"ChatOpenAI initialised", "white"))
-
-def get_item_details_node(offer_description : str = None) -> ItemDetails:
-    generator_model = model.with_structured_output(ItemDetails)
-    generator = get_item_details_prompt | generator_model
+    
+def get_multi_item_details_node(offer_description : str = None) -> Items:
+    generator_model = model.with_structured_output(Items)
+    generator = get_multi_item_details_prompt | generator_model
     flow_json = read_json(JSON_PATH)
-    option_paths = get_option_paths(flow_json)
-    category_list = get_propmt_list(option_paths)
     item_instructions = list_subroot_instructions(flow_json)
 
     response = generator.invoke({
@@ -35,11 +33,10 @@ def get_item_details_node(offer_description : str = None) -> ItemDetails:
         # "offer_context": offer_description
     })
 
-    if isinstance(response, ItemDetails):
+    if isinstance(response, Items):
         return response
     else:
         return None
 
 if __name__ == "__main__":
     description = "designed for the competitive player, this bundle provides crucial boosts for PvP battles, enhancing both offense and defense strategies. With upgrades tailored to skills and gear, players are positioned for success in their PvP endeavors."
-    print(get_item_details_node(item_description=description))
--- src/agents/item_agent/states/states.py
+++ src/agents/item_agent/states/states.py
@@ -6,3 +6,7 @@ class ItemDetails(BaseModel):
     description : str = Field(..., description="Intresting lore of the item")
     amount : int = Field(..., description="Amount of item")
     properties : str = Field(..., description="Properties of the item")
+    category : str = Field(..., description="Category of the item")
+
+class Items(BaseModel):
+    items: List[ItemDetails] = Field(..., description="List of items")
