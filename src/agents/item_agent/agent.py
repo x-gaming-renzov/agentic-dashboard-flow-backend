a572ba779a1529b87ed888410e8ae4a8f8bfba11
jeeravan <pranaypandit12@gmail.com>
2025-02-07 14:55:27 +0530

    stitch

--- src/agents/item_agent/agent.py
+++ src/agents/item_agent/agent.py
@@ -2,3 +2,8 @@ from langgraph.graph import StateGraph, START, END
 
 from .nodes.nodes import *
 from .utils.databases import *
+
+def get_items(offer_description: str):
+    items = get_multi_item_details_node(offer_description)
+    items_with_command = get_multi_item_commands(items)
+    return items_with_command
